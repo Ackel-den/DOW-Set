96a4767ade54fe056680c6ecc65b9788d75a26e0
Ackel-den(Denis) <gamedenis0@gmail.com>
2024-07-10 16:22:39 +0500

    AAAA

--- DOW-Set/ContentView.swift
+++ DOW-Set/ContentView.swift
@@ -1,24 +1,57 @@
 //
 //  ContentView.swift
-//  DOW-Set
-//
-//  Created by Денис Никитин on 10.07.2024.
+//  DOW(Set Game)
+
+//  D - Diamonds
+//  O - Ovals
+//  W - Waves
+
+//  Created by Денис Никитин on 02.07.2024.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var viewModel: ViewModel
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        AspectVGrid(items: viewModel.cardOnTable, aspectRatio: 2/3){card in
+            if !card.isMatched{
+                cardView(for: card).onTapGesture {
+                    viewModel.choose(card: card)
+                }
+            }
         }
-        .padding()
+        HStack(){
+            if !viewModel.theDeck.isEmpty{
+                AddButton().onTapGesture {
+                    viewModel.add()
+                }
+            }
+                TheDeck(number: viewModel.theDeck.count)
+        }
     }
+    @ViewBuilder
+    private func cardView(for card: DOWSetGameModel.Card) -> some View{
+        CardView(content: card)
+    }
+
 }
 
 #Preview {
-    ContentView()
+    let game = ViewModel()
+    return ContentView(viewModel: game)
 }
+
+
+/* БАГИ И НЕРЕШЁННЫЕ ЗАДАЧИ:
+    - невозможно сделать последнее добавление карт: Preview crash/index out of
+ range
+    - не выходит выбрать и убрать выбор с карты - она остаётся в памяти и
+ происходит проверка на сет
+    - решить переход на ScrolView, когда количество карт на столе = 19, так как чем больше карт
+ тем меньше становятся -> невозможно разглядеть элементы карт
+    - иногда попадаются одинаковые карты (???)
+    - решить начало новый игры
+    - даже если 12 карт на столе, View меняется так, будто на столе более 12 карт(!)
+*/
