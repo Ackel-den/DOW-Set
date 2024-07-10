d7ee017e77641271fed07d472904fd333220490f
Ackel-den(Denis) <gamedenis0@gmail.com>
2024-07-10 16:12:41 +0500

    first

---
+++ DOW-Set/DOW_SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DOW_SetApp.swift
+//  DOW-Set
+//
+//  Created by Денис Никитин on 10.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct DOW_SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
